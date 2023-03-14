65572ba91eb1ab5364327d526a2956206d567d90
Leonid ocampo <lnckocampo@yahoo.com>
2023-03-14 22:07:38 +0000

    cambios nuevos

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let pronoun;
+  let pronoun = ["the"];
+  let adj = ["great"];
+  let noun = ["jogger"];
+
+  for (let i = 0; (i = pronoun.length); i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; noun < adj.length; k++) {
+        console.log(pronoun[i] + adj[j] + adj[k]);
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
